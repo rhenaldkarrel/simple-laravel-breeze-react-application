6069bc196e7f050c600538389ed89bc2e9a75f7f
Rhenald <renaldkarel@gmail.com>
2023-05-11 12:19:51 +0700

    feat: get all cities api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::get('/countries', [CountryController::class, 'index']);
+
+Route::get('/cities', [CityController::class, 'index']);
