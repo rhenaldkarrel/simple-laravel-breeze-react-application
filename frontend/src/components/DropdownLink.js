ebf084bc37ee04a9624d13927ec76db8e725416d
Rhenald <renaldkarel@gmail.com>
2023-05-12 10:08:09 +0700

    feat: initial action button country

--- frontend/src/components/DropdownLink.js
+++ frontend/src/components/DropdownLink.js
@@ -14,11 +14,11 @@ const DropdownLink = ({ children, ...props }) => (
   </Menu.Item>
 )
 
-export const DropdownButton = ({ children, ...props }) => (
+export const DropdownButton = ({ children, className, ...props }) => (
   <Menu.Item>
     {({ active }) => (
       <button
-        className={`w-full text-left block px-4 py-2 
+        className={`${className} w-full text-left block px-4 py-2 
         text-sm leading-5 text-gray-700 ${active ? 'bg-gray-100' : ''}
          focus:outline-none transition duration-150 ease-in-out`}
         {...props}>
--- frontend/src/pages/country.js
+++ frontend/src/pages/country.js
@@ -1,4 +1,6 @@
 import Button from "components/Button";
+import Dropdown from "components/Dropdown";
+import { DropdownButton } from "components/DropdownLink";
 import Input from "components/Input";
 import Label from "components/Label";
 import AppLayout from "components/Layouts/AppLayout";
@@ -118,6 +120,37 @@ const Country = () => {
                   selector: (row) =>
                     new Date(row.created_at).toLocaleDateString(),
                   sortable: true,
+                },
+                {
+                  name: "Actions",
+                  button: true,
+                  cell: (row) => (
+                    <Dropdown
+                      align="right"
+                      width="48"
+                      trigger={
+                        <button className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
+                          <div>Action</div>
+                          <div className="ml-1">
+                            <svg
+                              className="fill-current h-4 w-4"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                          </div>
+                        </button>
+                      }
+                    >
+                      <DropdownButton onClick={() => {}}>Edit</DropdownButton>
+                      <DropdownButton className="text-red-400" onClick={() => {}}>Delete</DropdownButton>
+                    </Dropdown>
+                  ),
                 },
               ]}
               data={data}
